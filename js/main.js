456f21a64543414c5558bd1e1ba36ef077ce01ea
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-19 11:36:11 +0300

    Switch case and default

--- js/main.js
+++ js/main.js
@@ -7,9 +7,28 @@
 // var num;
 // num = 5;
 
-var num = 5;
+// var num = 5;
 
-console.log("Переменная:" + num);
+// console.log("Переменная:" + num);
 
 
-console.log("Math: " + (Math.PI + Math.max(5, 6, 4, 2)+ Math.min(2, -3,-1, 0)))
+// console.log("Math: " + (Math.PI + Math.max(5, 6, 4, 2)+ Math.min(2, -3,-1, 0)))
+
+var num = 4;
+
+switch(num) {
+    case 5:
+        console.log("num = 2");
+        break;
+        case 2:
+            console.log("num = 3");
+            break;
+            case 3:
+                console.log("num = 3");
+                break;
+                case 1:
+                    console.log("num = 1");
+                    break;
+default:
+    console.log("num default");
+}
